69c6bcf62eed2f7c668dfd38dc6399f5484e64e3
Puru Ujjlayn <146055626+PuruUjjlayn@users.noreply.github.com>
2024-09-27 15:16:14 +0530

    Update 3_Recommend Appartments.py

--- Pages/3_Recommend Appartments.py
+++ Pages/3_Recommend Appartments.py
@@ -5,11 +5,11 @@ import numpy as np
 
 st.set_page_config(page_title="Recommend Appartments")
 
-location_df = pickle.load(open(r'C:\Users\puruc\Downloads\render-projects\House-Price-Prediction\Pages\location_df.pkl','rb'))
+location_df = pickle.load(open('Pages\location_df.pkl','rb'))
 
-cosine_sim1 = pickle.load(open(r'C:\Users\puruc\Downloads\render-projects\House-Price-Prediction\Pages\cosine_sim1.pkl','rb'))
-cosine_sim2 = pickle.load(open(r'C:\Users\puruc\Downloads\render-projects\House-Price-Prediction\Pages\cosine_sim2.pkl','rb'))
-cosine_sim3 = pickle.load(open(r'C:\Users\puruc\Downloads\render-projects\House-Price-Prediction\Pages\cosine_sim3.pkl','rb'))
+cosine_sim1 = pickle.load(open('Pages\cosine_sim1.pkl','rb'))
+cosine_sim2 = pickle.load(open('Pages\cosine_sim2.pkl','rb'))
+cosine_sim3 = pickle.load(open('Pages\cosine_sim3.pkl','rb'))
 
 
 def recommend_properties_with_scores(property_name, top_n=5):
